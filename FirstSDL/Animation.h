52f2e382d5b2cd3cb1bcdbceeefb925bc05e6aa5
Erik Palenčík <erik.palencik@edgecom.eu>
2017-09-16 21:51:16 +0200

    commit init changes

---
+++ FirstSDL/Animation.h
@@ -0,0 +1,32 @@
+#pragma once
+#include <SDL.h>
+#include <SDL_main.h>
+#include "SdlWindow.h"
+
+class Animation
+{
+public:
+	Animation(rectangles* animatedRectangles);
+	~Animation();
+
+
+	void animate();
+	void disableAnimating();
+	void enableAnimating();
+
+private:
+
+	bool* goingRight;
+	bool* goingLeft;
+	bool* goingUp;
+	bool* goingDown;
+
+	rectangles* animatedObject;
+
+	SdlWindow* window;
+
+	bool* canAnimate;
+
+};
+
+
---
+++ FirstSDL/Source.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "LevelController.h"
+
+
+
+
+int main(int, char**) {
+
+	
+	LevelController* controller = new LevelController();
+	controller->run();
+	
+	
+	
+	return 0;
+}
